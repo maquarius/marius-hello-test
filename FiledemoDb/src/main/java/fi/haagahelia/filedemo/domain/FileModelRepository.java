ffa5eb528c94c3978e57e4cee2466e7d5d93c6f5
Marius van der Busse <mariusvanderbusse@hotmail.com>
2020-03-17 11:50:43 +0200

    changed structure

--- FiledemoDb/src/main/java/fi/haagahelia/filedemo/domain/FileModelRepository.java
+++
@@ -1,7 +0,0 @@
-package fi.haagahelia.filedemo.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface FileModelRepository extends CrudRepository<FileModel, Long> {
-
-}
